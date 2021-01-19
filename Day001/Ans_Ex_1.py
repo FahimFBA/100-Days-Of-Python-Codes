df729dfa2bcb0f0aa605b21132cff5563ab7f69a
FahimFBA <fahimbinamin@gmail.com>
2021-01-19 11:47:53 +0600

    \n character

--- Day001/Ans_Ex_1.py
+++
@@ -1,4 +0,0 @@
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
-# print('print("what to print")')
--- Day001/NewLine.py
+++
@@ -1,3 +0,0 @@
-print("This is a sample of newline using the newline character.\nThis is a sample of newline using the newline character.\nThis is a sample of newline using the newline character.")
-
-# \n works as a newline character
