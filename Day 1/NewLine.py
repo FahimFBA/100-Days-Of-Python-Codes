6c47c1886c631b1a9c568ca04ca87da7b707fb3e
FahimFBA <fahimbinamin@gmail.com>
2021-01-19 11:40:43 +0600

    NewLine character

---
+++ Day 1/NewLine.py
@@ -0,0 +1,3 @@
+print("This is a sample of newline using the newline character.\nThis is a sample of newline using the newline character.\nThis is a sample of newline using the newline character.")
+
+# \n works as a newline character
