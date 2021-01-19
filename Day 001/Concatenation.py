74bb884504e42c825b8a7490870f35f7a7901fe3
FahimFBA <fahimbinamin@gmail.com>
2021-01-19 12:04:14 +0600

    Concatenation in strings

---
+++ Day 001/Concatenation.py
@@ -0,0 +1,16 @@
+print("Hello" + "Fahim Bin Amin") # Output: HelloFahim Bin Amin
+
+# There are 3 ways to add space between the two strings
+
+# 1st Way :
+
+print("Hello " + "Fahim Bin Amin") # Output: Hello Fahim Bin Amin
+
+# 2nd Way :
+
+print("Hello" + " Fahim Bin Amin") # Output: Hello Fahim Bin Amin
+
+
+# 3rd Way :
+
+print("Hello" + " " + "Fahim Bin Amin") # Output: Hello Fahim Bin Amin
