c8b2147337336100b0fa3da4ad85de8c5cba7f72
FahimFBA <fahimbinamin@gmail.com>
2021-01-19 13:02:02 +0600

    debugging done

--- Day 001/Debug.py
+++ Day 001/Debug.py
@@ -2,3 +2,19 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the " + '"+"' + " sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+
+
+# Another method with the explanations
+
+#1. Missing double quotes before the word Day.
+print("Day 1 - String Manipulation")
+
+#2. Outer double quotes changed to single quotes.
+print('String Concatenation is done with the "+" sign.')
+
+#3. Extra indentation removed
+print('e.g. print("Hello " + "world")')
+
+#4. Extra ( in print function removed.
+print("New lines can be created with a backslash and n.")
