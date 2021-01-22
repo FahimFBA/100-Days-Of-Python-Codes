9d9c5b4d69d2e7b47b1d037b119c38cef3797f23
FahimFBA <fahimbinamin@gmail.com>
2021-01-22 21:06:26 +0600

    documented

--- Day 001/InputFunction.py
+++ Day 001/InputFunction.py
@@ -1,1 +1,10 @@
+# input() will get user input in console
+# then print() will print the word "Hello" and the user input
+
+
+
 print('Hello ' + input('What is your name? ') + "!")
+
+
+# for adding a full line as comment, select the full line and press Ctrl + / in windows
+# for adding a full line as comment, select the full line and press Command + /  in Mac
