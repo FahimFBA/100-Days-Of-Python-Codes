015371a67124d7a9fb4d3642437ddd7545c73bb8
FahimFBA <fahimbinamin@gmail.com>
2021-01-23 20:55:11 +0600

    exercise 2

---
+++ Day 001/Ans_Ex_2.py
@@ -0,0 +1,11 @@
+user_input = input()
+length = len(user_input)
+print(length)
+
+# Another method
+#This code prints the number of characters in a user's name.
+# print( len( input("What is your name? ") ) )
+#Notes
+#If input was "Jack"
+#1st: print(len("Jack"))
+#2nd: print(4)
