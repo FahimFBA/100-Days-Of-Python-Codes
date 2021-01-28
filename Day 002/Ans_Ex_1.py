e5745561dac6de9ed53cb21710a32d9c8dce474e
FahimFBA <fahimbinamin@gmail.com>
2021-01-28 21:41:12 +0600

    Ex 1 solved

---
+++ Day 002/Ans_Ex_1.py
@@ -0,0 +1,51 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+
+# Checking the type of two_digit_number
+
+print(type(two_digit_number))
+
+# Getting the first and second digit from the two_digit_numer
+
+first = (two_digit_number[0]) # getting the digit from index 0
+first = int(first) # converting the digit of index 0 from string to int
+
+second = (two_digit_number[1]) # getting the digit from index 1
+second = int(second) # converting the digit of index 1 from string
+
+result = (first+second) # getting the final result
+
+print(result)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Another method 
+
+# 🚨 Don't change the code below 👇
+# two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+#Check the data type of two_digit_number
+# print(type(two_digit_number))
+
+#Get the first and second digits using subscripting then convert string to int.
+# first_digit = int(two_digit_number[0])
+# second_digit = int(two_digit_number[1])
+
+#Add the two digits togeter
+# two_digit_number = first_digit + second_digit
+
+# print(two_digit_number)
+
+
+
+
+
