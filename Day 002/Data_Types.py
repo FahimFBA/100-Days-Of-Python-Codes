fcee7311f4ff9b15c441456d102a478128ad80fa
FahimFBA <fahimbinamin@gmail.com>
2021-01-28 20:49:51 +0600

    Data Types

---
+++ Day 002/Data_Types.py
@@ -0,0 +1,20 @@
+
+# String
+
+print("Hello" [0]) # printing the value of index 0 from the word "Hello"
+print("Hello" [4])
+
+
+# Integer
+
+print(123+345)
+print(123_456_789) # another form of 123,456,789
+
+# Float
+
+3.14159
+
+# Boolean
+
+True
+False
