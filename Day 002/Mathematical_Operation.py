38d50e573a791bfb1156c5f1d6118936b301ec0a
FahimFBA <fahimbinamin@gmail.com>
2021-01-29 12:35:30 +0600

    operators

---
+++ Day 002/Mathematical_Operation.py
@@ -0,0 +1,32 @@
+
+
+
+print( 6 / 3 ) # Will always provide float value
+print( 5 + 2 )
+print( 9 * 2 )
+print( 7 - 4 )
+print( 3 * 2 )
+print( 2 ** 3 ) # 2^3
+
+# Sequence
+
+# PEMDAS   
+
+# Parentheses ()
+# Exponents **
+# Multiplication *
+# Division /
+# Addition +
+# Substraction -
+
+
+# PEMDASLR
+
+# ()
+# **
+# * /
+# + -
+
+print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
