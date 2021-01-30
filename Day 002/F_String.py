7ec4481dfea57f7012fb6f8ad1120570066acb01
FahimFBA <fahimbinamin@gmail.com>
2021-01-31 01:12:28 +0600

    f-string added

---
+++ Day 002/F_String.py
@@ -0,0 +1,9 @@
+score = 0
+height = 1.8
+isWinning = True
+
+#f-String
+
+# f-string automatically converts all other data types into string within curly braces {}  
+
+print(f"Your score is {score}, your height is {height}, your are winning is {isWinning}")
--- Day 002/Number_Manipulation.py
+++ Day 002/Number_Manipulation.py
@@ -5,3 +5,7 @@ print(8//3) # Provides the int value without fraction
 print(int(8/3)) # Provides the int value without fraction
 
 print(round(8/3)) # Provides the round figure value
+
+print(round(8/3 , 2)) # Provides the round figure value till 2 values after the radix point
+
+print(round(2.666666666666666666666666666666666666666666666, 2))
