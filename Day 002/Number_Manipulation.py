f9383605d901ad15e58e317fbd0c96a110ab49a3
FahimFBA <fahimbinamin@gmail.com>
2021-01-31 00:57:56 +0600

    Number manipulation

---
+++ Day 002/Number_Manipulation.py
@@ -0,0 +1,7 @@
+print(8/3) # Provides full value with fraction
+
+print(8//3) # Provides the int value without fraction
+
+print(int(8/3)) # Provides the int value without fraction
+
+print(round(8/3)) # Provides the round figure value
