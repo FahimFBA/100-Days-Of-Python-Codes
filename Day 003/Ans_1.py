86278c271464a9ea58de130f36482b9813b9039e
FahimFBA <fahimbinamin@gmail.com>
2021-02-03 21:51:49 +0600

    correction on Ex 1

--- Day 003/Ans_1.py
+++ Day 003/Ans_1.py
@@ -4,7 +4,7 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 👇
 
-if number%2==0:
+if number%2!=0:
     print("This is an odd number.")
 else:
     print("This is a even number.")
