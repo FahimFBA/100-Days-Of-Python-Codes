5809574dc08b1e00cd14d3b9b4e786076139b5ee
FahimFBA <fahimbinamin@gmail.com>
2021-02-03 21:25:36 +0600

    comparison in python

---
+++ Day 003/Comparison.py
@@ -0,0 +1,9 @@
+print('Welcome to the rollercoaster!')
+height = int(input('What is your height in centimeter? '))
+
+# Comparison is starting from here
+
+if height >= 120:
+    print('You can ride the rollercoaster!')
+else:
+    print('Sorry! You have to grow taller before you can ride.')
